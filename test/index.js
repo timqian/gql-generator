15ab3c8613c6e13b61eaf36340f543bdffe8bc8d
timqian <timqian92@qq.com>
2018-12-19 13:14:52 +0800

    renaming

--- test/index.js
+++ test/index.js
@@ -3,12 +3,12 @@ require('should');
 
 test('validate generated queries', async () => {
   cp.execSync('node index.js --schemaFilePath ./example/sampleTypeDef.graphql --destDirPath ./example/output');
-  const query = require('../example/output');
-  query.mutations.signin.indexOf('signin').should.not.equal(-1);
+  const queries = require('../example/output');
+  queries.mutations.signin.indexOf('signin').should.not.equal(-1);
 });
 
 test('limt depth', async () => {
   cp.execSync('node index.js --schemaFilePath ./example/sampleTypeDef.graphql --destDirPath ./example/output2 --depthLimit 1');
-  const query = require('../example/output2');
-  query.mutations.signup.indexOf('createdAt').should.equal(-1);
+  const queries = require('../example/output2');
+  queries.mutations.signup.indexOf('createdAt').should.equal(-1);
 });
