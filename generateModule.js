6ac5e45cb91fe8d4fbed146101475d1029f450e9
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2020-05-17 13:10:59 +0200

    more generation

--- generateModule.js
+++ generateModule.js
@@ -36,60 +36,116 @@ shelljs.mkdir('-p', `${projectMainPath}/src/modules/${moduleName}/graphql`);
 
 const Handlebars = require('handlebars');
 
-const createModuleGraphql = () => {
-  const moduleGraphqlTemplate = fs.readFileSync(path.join(__dirname, './templates/module.graphql'), 'utf8');
+const modules = shelljs.ls(`${projectMainPath}/src/modules/`).map((p) => ({ name: p }));
 
-  const generateIndex = () => Handlebars.compile(moduleGraphqlTemplate)({});
+const accounts = modules.find((m) => m.name === 'Accounts');
+accounts.types = true;
+accounts.queries = [{ name: 'me' }, { name: 'notMe' }];
+accounts.mutations = [{ name: 'UserChangeName' }];
+if (modules.inventory) {
+  modules.inventory.queries = [{ name: 'inventoryGetName' }];
+}
 
-  fs.writeFileSync(
-    path.join(`${projectMainPath}/src/modules/${moduleName}/graphql/`, `${moduleName}.graphql`),
-    generateIndex()
-  );
-};
+modules.forEach((module) => {
 
-createModuleGraphql();
+  const moduleName = module.name
+  const createModuleResolvers = () => {
+    const template = fs.readFileSync(path.join(__dirname, './templates/moduleResolvers.handlebars'), 'utf8');
+    const context = { types: true, queries: [], mutations: [], moduleName };
+    // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
+    const generateIndex = () => Handlebars.compile(template)(context);
 
-const createModuleResolvers = () => {
-  const template = fs.readFileSync(path.join(__dirname, './templates/moduleResolvers.handlebars'), 'utf8');
-  const context = { types: true, queries: [], mutations: [], moduleName };
-  // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
-  const generateIndex = () => Handlebars.compile(template)(context);
+    fs.writeFileSync(
+      path.join(`${projectMainPath}/src/modules/${moduleName}/graphql/`, `${moduleName}Resolvers.ts`),
+      generateIndex()
+    );
+  };
 
-  fs.writeFileSync(
-    path.join(`${projectMainPath}/src/modules/${moduleName}/graphql/`, `${moduleName}Resolvers.ts`),
-    generateIndex()
-  );
-};
+  createModuleResolvers();
+})
 
-createModuleResolvers();
+// const createModuleGraphql = () => {
+//   const moduleGraphqlTemplate = fs.readFileSync(path.join(__dirname, './templates/module.graphql'), 'utf8');
+//
+//   const generateIndex = () => Handlebars.compile(moduleGraphqlTemplate)({});
+//
+//   fs.writeFileSync(
+//     path.join(`${projectMainPath}/src/modules/${moduleName}/graphql/`, `${moduleName}.graphql`),
+//     generateIndex()
+//   );
+// };
+//
+// createModuleGraphql();
 
 
 const createGlobalResolvers = () => {
   const template = fs.readFileSync(path.join(__dirname, './templates/resolvers.handlebars'), 'utf8');
-  const context = { modules: [{name: "Inventory"}] };
+  const context = { modules: [{ name: 'Inventory' }] };
   // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
   const generateIndex = () => Handlebars.compile(template)(context);
 
-  fs.writeFileSync(
-    path.join(`${projectMainPath}/src/graphql/`, `resolvers.ts`),
-    generateIndex()
-  );
+  fs.writeFileSync(path.join(`${projectMainPath}/src/graphql/`, `resolvers.ts`), generateIndex());
 };
 
 createGlobalResolvers();
 
-
 const createCombineSchemas = () => {
   const template = fs.readFileSync(path.join(__dirname, './templates/combineSchemas.handlebars'), 'utf8');
-  const context = { modules: [{name: "Inventory"}, {name: "Accounts"}, {name: "Products"}] };
+  const context = { modules };
   // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
   const generateIndex = () => Handlebars.compile(template)(context);
 
-  fs.writeFileSync(
-    path.join(`${projectMainPath}/src/graphql/helpers`, `combineSchemas.ts`),
-    generateIndex()
-  );
+  fs.writeFileSync(path.join(`${projectMainPath}/src/graphql/helpers`, `combineSchemas.ts`), generateIndex());
 };
 
 createCombineSchemas();
 
+const createTypes = () => {
+  const template = fs.readFileSync(path.join(__dirname, './templates/types.handlebars'), 'utf8');
+  const context = { modules };
+  // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
+  const generateIndex = () => Handlebars.compile(template)(context);
+
+  fs.writeFileSync(path.join(`${projectMainPath}/src/`, `types.ts`), generateIndex());
+};
+
+createTypes();
+
+const createStartupConfig = () => {
+  const template = fs.readFileSync(path.join(__dirname, './templates/startupConfig.handlebars'), 'utf8');
+  const context = { modules };
+  // const context = { types: true, queries: [{name: "myQuery"}], mutations: [{name: "myMutation"}], moduleName };
+  const generateIndex = () => Handlebars.compile(template)(context);
+
+  fs.writeFileSync(path.join(`${projectMainPath}/src/`, `startupConfig.ts`), generateIndex());
+};
+
+createStartupConfig();
+
+const createIModuleNameContexts = () => {
+  const template = fs.readFileSync(path.join(__dirname, './templates/IModuleNameContext.handlebars'), 'utf8');
+
+  modules.forEach(({ name }) => {
+    const context = { moduleName: name };
+
+    const generateIndex = () => Handlebars.compile(template)(context);
+
+    fs.writeFileSync(path.join(`${projectMainPath}/src/modules/${name}/`, `I${name}Context.ts`), generateIndex());
+  });
+};
+
+createIModuleNameContexts();
+
+const createGetModuleNameContexts = () => {
+  const template = fs.readFileSync(path.join(__dirname, './templates/getModuleNameContext.handlebars'), 'utf8');
+
+  modules.forEach(({ name }) => {
+    const context = { moduleName: name };
+
+    const generateIndex = () => Handlebars.compile(template)(context);
+
+    fs.writeFileSync(path.join(`${projectMainPath}/src/modules/${name}/`, `get${name}Context.ts`), generateIndex());
+  });
+};
+
+createGetModuleNameContexts();
