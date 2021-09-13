86912d774359dd47ba8d4b0ae6a6fc11779eb4c5
Misha Syrbu <mihaisyrbu91@gmail.com>
2021-09-13 11:43:41 +0300

    fix: Correct display of union child, fix empty braces when no fields on selected deep

--- test/index.js
+++ test/index.js
@@ -16,7 +16,7 @@ test('limit depth', async () => {
 test('excludes deprecated fields by default', async () => {
   cp.execSync('node index.js --schemaFilePath ./example/sampleTypeDef.graphql --destDirPath ./example/output3 --depthLimit 1');
   const queries = require('../example/output3');
-  should(typeof queries.queries.user).be.exactly("string");
+  should(typeof queries.queries.user).be.exactly('string');
   should(queries.queries.members === undefined).be.true();
   should(queries.mutations.sendMessage === undefined).be.true();
 
@@ -33,20 +33,23 @@ test('excludes deprecated fields by default', async () => {
             ... on Member {
                 address
             }
+            ... on Premium {
+                location
+            }
         }
     }
-}`
+}`;
 
-  should(queries.queries.user === expected).be.true();  
+  should(queries.queries.user === expected).be.true();
 });
 
 test('includes deprecated fields with includeDeprecatedFields flag', async () => {
   cp.execSync('node index.js --schemaFilePath ./example/sampleTypeDef.graphql --destDirPath ./example/output4 --depthLimit 1 --includeDeprecatedFields');
   const queries = require('../example/output4');
 
-  should(typeof queries.queries.user).be.exactly("string");
-  should(typeof queries.queries.members).be.exactly("string");
-  should(typeof queries.mutations.sendMessage).be.exactly("string");
+  should(typeof queries.queries.user).be.exactly('string');
+  should(typeof queries.queries.members).be.exactly('string');
+  should(typeof queries.mutations.sendMessage).be.exactly('string');
 
   const expected = `query user($language: String, $id: Int!){
     user(id: $id){
@@ -61,9 +64,48 @@ test('includes deprecated fields with includeDeprecatedFields flag', async () =>
             ... on Member {
                 address
             }
+            ... on Premium {
+                location
+            }
         }
         address
     }
-}`
+}`;
   should(queries.queries.user === expected).be.true();
+});
+
+test('includes nested in union types', async () => {
+  cp.execSync('node index.js --schemaFilePath ./example/sampleTypeDef.graphql --destDirPath ./example/output5 --depthLimit 2');
+  const queries = require('../example/output5');
+
+  should(typeof queries.queries.user).be.exactly('string');
+  should(queries.queries.members === undefined).be.true();
+  should(queries.mutations.sendMessage === undefined).be.true();
+
+  const expected = `query user($language: String, $id: Int!){
+    user(id: $id){
+        id
+        username
+        email
+        createdAt
+        context{
+            domain
+        }
+        details{
+            ... on Guest {
+                region(language: $language)
+            }
+            ... on Member {
+                address
+            }
+            ... on Premium {
+                location
+                card{
+                    number
+                }
+            }
+        }
+    }
+}`;
+  should(queries.queries.user).be.equal(expected);
 });
